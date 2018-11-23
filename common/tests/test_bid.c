15394685c2e3a5d3587b926653b3f5e86cb192c6
bodnartibi <piratetibi@gmail.com>
2018-11-23 23:01:50 +0100

    common/bid: add tests

--- common/tests/test_bid.c
+++ common/tests/test_bid.c
@@ -38,9 +38,42 @@ void test_set_right_bid()
   assert(qn == q);
 }
 
+void test_new_bid()
+{
+  Bid bid;
+  unsigned int f, q, fn, qn;
+
+  f = 2;
+  q = 5;
+  assert(bid.SetBid(f, q));
+  bid.GetBid(fn, qn);
+  assert(fn == f);
+  assert(qn == q);
+
+  f = 3;
+  q = 5;
+  assert(bid.SetBid(f, q));
+  bid.GetBid(fn, qn);
+  assert(fn == f);
+  assert(qn == q);
+
+  f = 3;
+  q = 5;
+  assert(!bid.SetBid(f, q));
+
+  f = 2;
+  q = 6;
+  assert(!bid.SetBid(f, q));
+
+  f = 4;
+  q = 3;
+  assert(!bid.SetBid(f, q));
+}
+
 int
 main (int argc, char *argv[])
 {
   test_set_right_bid();
   test_set_bad_bid();
+  test_new_bid();
 }
