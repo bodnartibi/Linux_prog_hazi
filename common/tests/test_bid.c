196f87a305c4e331db7c4c093e9db4c0a2ce9d0e
bodnartibi <piratetibi@gmail.com>
2018-11-23 23:11:03 +0100

    common/bid: test refactor

--- common/tests/test_bid.c
+++ common/tests/test_bid.c
@@ -3,71 +3,46 @@
 
 #define NDEBUG
 
+void _set_should_fail(Bid *bid, unsigned int f, unsigned int q)
+{
+  assert(!bid->SetBid(f, q));
+}
+
+void _set_should_pass(Bid *bid, unsigned int f, unsigned int q)
+{
+  unsigned int fn, qn;
+
+  assert(bid->SetBid(f, q));
+  bid->GetBid(fn, qn);
+  assert(fn == f);
+  assert(qn == q);
+}
+
 void test_set_bad_bid()
 {
   Bid bid;
-  unsigned int f, q, fn, qn;
-
-  f = 0;
-  q = 5;
-  assert(!bid.SetBid(f, q));
-
-  f = 2;
-  q = 0;
-  assert(!bid.SetBid(f, q));
-
-  f = 8;
-  q = 5;
-  assert(!bid.SetBid(f, q));
-
-  f = 0;
-  q = 5;
-  assert(!bid.SetBid(f, q));
+  _set_should_fail(&bid, 0, 5);
+  _set_should_fail(&bid, 2, 0);
+  _set_should_fail(&bid, 8, 5);
+  _set_should_fail(&bid, 0, 5);
 }
 
 void test_set_right_bid()
 {
   Bid bid;
-  unsigned int f, q, fn, qn;
-
-  f = 2;
-  q = 5;
-  assert(bid.SetBid(f, q));
-  bid.GetBid(fn, qn);
-  assert(fn == f);
-  assert(qn == q);
+  _set_should_pass(&bid, 2, 5);
 }
 
 void test_new_bid()
 {
   Bid bid;
-  unsigned int f, q, fn, qn;
 
-  f = 2;
-  q = 5;
-  assert(bid.SetBid(f, q));
-  bid.GetBid(fn, qn);
-  assert(fn == f);
-  assert(qn == q);
+  _set_should_pass(&bid, 2, 5);
+  _set_should_pass(&bid, 3, 5);
 
-  f = 3;
-  q = 5;
-  assert(bid.SetBid(f, q));
-  bid.GetBid(fn, qn);
-  assert(fn == f);
-  assert(qn == q);
-
-  f = 3;
-  q = 5;
-  assert(!bid.SetBid(f, q));
-
-  f = 2;
-  q = 6;
-  assert(!bid.SetBid(f, q));
-
-  f = 4;
-  q = 3;
-  assert(!bid.SetBid(f, q));
+  _set_should_fail(&bid, 3, 5);
+  _set_should_fail(&bid, 2, 6);
+  _set_should_fail(&bid, 4, 3);
 }
 
 int
